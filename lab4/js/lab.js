fcba97f89620cf4dac36c98828f46a3a7c6fb123
silveriaaa <emblanco.usa@gmail.com>
2025-04-21 21:38:10 -0700

    lab 3 and lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,38 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Number Guessing Game
+// An interactive number guessing game in the browser
+// Emily Blanco
+// April 2025
+
+// I. Game Planning
+//    A. Define the game rules
+//      - Player must guess a number within a certain range
+//      - Computer gives hints: "Too high", "Too low", or "Correct"
+//    B. Determine user interface (UI) style
+//      - sumple web page with input box and feedback area
+
+// II. Set Up Game Environment
+//    A. Create basic HTML structure
+//    B. Link CSS for styling
+//    C. Link JavaScript file for game logic
+
+// III. Game Initialization
+//    A. Generate a random number within a desired range (e.g., 1-100)
+//    B. Set up initial variables (e.g., guess count, max attempts)
+//    C. Display Instruction to the player
+
+// IV. User Input Handling
+//    A. Capture number input from user
+//    B. Validate input
+//      - Ensure it's a number
+//      - Ensure it's within the specified range
+
+// V. Game Logic & Feedback
+//    A. Compare user's guess with the generated number
+//      - If guess is correct show succes message
+//      - If guess is too high show "Too high" or "Too low" message, show hint
+//    B. Track number of guesses
+//    C. Limit the number of attempts
+
 
 // Constants
 
