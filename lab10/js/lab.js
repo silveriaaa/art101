134f7b471ff14d13a2044c9c01f8c36f95db4cd3
silveriaaa <emblanco.usa@gmail.com>
2025-05-15 23:04:42 -0700

    blah

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
+// index.js - appends new elements to an output div
 // Author: Emily Blanco
-// Date:
+// Date: May 2025
 
 // Create a webchat simulation with an input field and a button to add a message to the convo
 
@@ -11,28 +11,23 @@
 
 // user types something in the text boc
 
-// on clck event listener
-$("action").click( 
-    function() {
-      
-  // what will happen when button is clicked
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        const min = 3;
+        const max = 100;
+        const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+        // Get a random starting index to slice the text
+        const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+        // Generate the random text
+        return text.slice(randStart, randStart + randLen)
+    }
+// add an event listener to the button
+  $("#make-convo").click(function() {
+  });
+// get new fake dialogue
+const newText = generateRandomText();
 
-  // make the button copy the text
+// append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
 
-  // find the box
-
-  //usertext=$("#userinput").val("blahblah")
-
-  // and its contents
-
-  // copy the contents
-    let usertext=$("#userinput").val();
-  console.log(usertext);
-
-  // copy where? div convo
-
-  $("#convo").append(usertext+"<br>");
-
-  }
-)
 
