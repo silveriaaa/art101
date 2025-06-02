11990594f100a7c12313008b33c5d47278348d19
silveriaaa <emblanco.usa@gmail.com>
2025-06-01 21:29:35 -0700

    meow

---
+++ lab14/js/lab.js
@@ -0,0 +1,21 @@
+// index.js - Loops
+// Author: Emily Blanco
+// Date: May 2025
+
+
+
+// loop 1-100
+for (let i = 1; i <= 100; i++) {
+  if (i%3 == 0) {
+    console.log("FizzBuzz");
+  } else if (i%3 == 0) {
+    console.log("Fizz");
+  } else if (i%5 == 0) {
+    console.log("Buzz");
+  } else if (i%7 == 0) {
+    console.log("Boom");
+  }
+}
+
+
+ 
