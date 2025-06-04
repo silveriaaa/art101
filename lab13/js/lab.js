afcd2a9d7780a707cb23ca0411e76efe7064362f
silveriaaa <emblanco.usa@gmail.com>
2025-06-03 19:53:37 -0700

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -4,17 +4,30 @@
 
 
 
-// loop 1-100
+// Use a for loop to print all the numbers from 1 to 100. Test and debug.
 for (let i = 1; i <= 100; i++) {
-  if (i%3 == 0) {
-    console.log("FizzBuzz");
-  } else if (i%3 == 0) {
-    console.log("Fizz");
-  } else if (i%5 == 0) {
-    console.log("Buzz");
-  } else if (i%7 == 0) {
-    console.log("Boom");
+  // For numbers which are multiples of both 3 and 5, print “FizzBuzz” instead.
+  if(i % 15 == 0) {
+    console.log("FizzBuzz!");
+    $("#output").append("<p>FizzBuzz!</p>");
   }
+  // Add an if statement: if the number is a multiple of 3, print “Fizz” instead of the number.
+  else if (i % 3 == 0) {
+    console.log("Fizz!");
+    $("#output").append("<p>Fizz!</p>");
+  }
+  // Add an else if condition: If the number is a multiple of 5, print “Buzz” instead of the number.
+  else if(i % 5 == 0) {
+    console.log("Buzz!");
+    $("#output").append("<p>Buzz!</p>");
+    
+  }
+
+  else {
+    console.log(i);
+    $("#output").append(i);
+  }
+  
 }
 
 
